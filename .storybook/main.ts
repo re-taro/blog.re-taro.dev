f9d759bc46e50f5e795890c17eb65aae4a19872a
Rintaro Itokawa <me@re-taro.dev>
2024-04-24 22:17:12 +0900

    build(feat): setup Storybook (#15)

---
+++ .storybook/main.ts
@@ -0,0 +1,29 @@
+import type { StorybookConfig } from "storybook-framework-qwik";
+
+const config: StorybookConfig = {
+	stories: ["../src/**/*.stories.@(ts|tsx)"],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions",
+		"@storybook/addon-a11y",
+		"@storybook/addon-actions",
+		"@storybook/addon-backgrounds",
+		"@chromatic-com/storybook",
+	],
+	framework: {
+		name: "storybook-framework-qwik",
+	},
+	core: {
+		renderer: "storybook-framework-qwik",
+	},
+	docs: {
+		autodocs: "tag",
+	},
+	staticDirs: ["../public"],
+	viteFinal: (config) => {
+		return config;
+	},
+};
+
+export default config;
