ce77d583cfb87eb449b5aaecd97acd35b338c267
Rintaro Itokawa <me@re-taro.dev>
2024-04-27 23:35:03 +0900

    feat: add tokens and global config

--- src/root.tsx
+++ src/root.tsx
@@ -39,6 +39,9 @@ export default component$(() => {
 				<link rel="icon" href="/favicon.ico" sizes="any" />
 				<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
 				<link rel="stylesheet" href={styles} />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Noto+Sans+JP:wght@400;600&display=swap" />
 				<RouterHead />
 			</head>
 			<body lang="ja">
---
+++ src/styles/colors.ts
@@ -0,0 +1,28 @@
+import { defineTokens } from "@pandacss/dev";
+
+export const colors = defineTokens.colors({
+	bg: {
+		main: {
+			value: "#0b1215",
+		},
+		secondary: {
+			value: "#232a2c",
+		},
+		teriary: {
+			value: "#3c4144",
+		},
+	},
+	text: {
+		main: {
+			value: "#ced0d0",
+		},
+		secondary: {
+			value: "#b4b5b5",
+		},
+	},
+	border: {
+		main: {
+			value: "#909191",
+		},
+	},
+});
