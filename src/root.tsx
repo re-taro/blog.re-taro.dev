038776416a65e61cbd006f4ab197288e1426ebd4
Rintaro Itokawa <me@re-taro.dev>
2024-05-13 12:13:29 +0900

    refactor: remove meta from the root

--- src/root.tsx
+++ src/root.tsx
@@ -13,35 +13,6 @@ export default component$(() => {
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width" />
-				<meta
-					name="description"
-					content="Rintaro Itokawa(re-taro)のブログです。"
-				/>
-				<meta property="og:title" content="blog.re-taro.dev" />
-				<meta
-					property="og:description"
-					content="Rintaro Itokawa(re-taro)のブログです。"
-				/>
-				<meta property="og:url" content="https://blog.re-taro.dev" />
-				<meta property="og:image" content="https://og.re-taro.dev?title=blog.re-taro.dev" />
-				<meta property="og:image:alt" content="Rintaro Itokawa(re-taro)のブログです。" />
-				<meta property="og:type" content="website" />
-				<meta property="twitter:card" content="summary_large_image" />
-				<meta
-					property="twitter:title"
-					content="blog.re-taro.dev"
-				/>
-				<meta
-					property="twitter:description"
-					content="Rintaro Itokawa(re-taro)のブログです。"
-				/>
-				<meta property="twitter:image" content="https://og.re-taro.dev?title=blog.re-taro.dev" />
-				<meta
-					property="twitter:image:alt"
-					content="Rintaro Itokawa(re-taro)のブログです。"
-				/>
-				<meta property="twitter:site" content="@re_taro_" />
-				<meta property="twitter:creator" content="@re_taro_" />
 				<link rel="icon" href="/favicon.ico" sizes="any" />
 				<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
 				<link rel="stylesheet" href={styles} />
