9f843150fe28a0dbe04d4ccd2078be492d4179e3
Rintaro Itokawa <me@re-taro.dev>
2024-05-20 11:08:55 +0900

    fix: fix gtm settings (#85)

--- src/root.tsx
+++ src/root.tsx
@@ -14,24 +14,21 @@ export default component$(() => {
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<QwikPartytown forward={["gtag", "dataLayer.push"]} />
-				<script
-					async
-					type="text/partytown"
-					src="https://www.googletagmanager.com/gtag/js?id=G-048WY1ZY4E"
-				>
-				</script>
-				<script
-					type="text/partytown"
-					dangerouslySetInnerHTML="
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments);}
-						gtag('js', new Date());
-
-						gtag('config', 'G-048WY1ZY4E');
-					"
-				>
-				</script>
+				{import.meta.env.PROD && (
+					<>
+						<QwikPartytown forward={["dataLayer.push"]} />
+						<script
+							type="text/partytown"
+							dangerouslySetInnerHTML={`
+								(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+								new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+								j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+								'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+								})(window,document,'script','dataLayer','GTM-KDDWP2FS');
+							`}
+						/>
+					</>
+				)}
 				<link rel="icon" href="/favicon.ico" sizes="any" />
 				<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
 				<link rel="stylesheet" href={styles} />
