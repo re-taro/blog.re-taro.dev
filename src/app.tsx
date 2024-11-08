38e81b3b603e6f6c5ccd1dc8d690d8f5755704ec
Rintaro Itokawa <me@re-taro.dev>
2024-11-08 18:25:23 +0900

    fix: move GTM loader to client side (#493)

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,8 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import "./global.css";
 import { MetaProvider } from "@solidjs/meta";
+import { Assets } from "solid-js/web";
+import { partytownSnippet } from "@builder.io/partytown/integration";
 import { css } from "./styled-system/css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
@@ -36,11 +38,29 @@ const Layout: Component<Props> = (props) => {
 export default function App() {
 	return (
 		<Router root={props => (
-			<MetaProvider>
-				<Layout>
-					<Suspense fallback={null}>{props.children}</Suspense>
-				</Layout>
-			</MetaProvider>
+			<>
+				{import.meta.env.PROD && (
+					<Assets>
+						<script>{partytownSnippet({ forward: ["dataLayer.push"] })}</script>
+						<script
+							type="text/partytown"
+						>
+							{`
+							(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+							})(window,document,'script','dataLayer','GTM-KDDWP2FS');
+						`}
+						</script>
+					</Assets>
+				)}
+				<MetaProvider>
+					<Layout>
+						<Suspense fallback={null}>{props.children}</Suspense>
+					</Layout>
+				</MetaProvider>
+			</>
 		)}
 		>
 			<FileRoutes />
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -9,29 +9,6 @@ export default createHandler(() => (
 				<head>
 					<meta charset="utf-8" />
 					<meta content="width=device-width, initial-scale=1" name="viewport" />
-					{import.meta.env.PROD && (
-						<>
-							<script
-								// eslint-disable-next-line solid/no-innerhtml
-								innerHTML={`
-									partytown = {
-										forward: ["dataLayer.push"],
-									}
-								`.trim()}
-							/>
-							<script
-								// eslint-disable-next-line solid/no-innerhtml
-								innerHTML={`
-									(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-									new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-									j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-									'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-									})(window,document,'script','dataLayer','GTM-KDDWP2FS');
-								`.trim()}
-								type="text/partytown"
-							/>
-						</>
-					)}
 					<link href="/favicon.ico" rel="icon" sizes="any" />
 					<link href="/favicon.svg" rel="icon" type="image/svg+xml" />
 					<link href={styles} rel="stylesheet" />
