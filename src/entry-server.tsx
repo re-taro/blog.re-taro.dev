f6af2c03b763333f3d079e9dc8c03233fe5870fa
Rintaro Itokawa <me@re-taro.dev>
2024-11-08 16:34:35 +0900

    fix: fix gtm loader

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -11,20 +11,23 @@ export default createHandler(() => (
 					<meta content="width=device-width, initial-scale=1" name="viewport" />
 					{import.meta.env.PROD && (
 						<>
-							<script innerText={`
+							<script
+								// eslint-disable-next-line solid/no-innerhtml
+								innerHTML={`
 									partytown = {
 										forward: ["dataLayer.push"],
 									}
-								`}
+								`.trim()}
 							/>
 							<script
-								innerText={`
+								// eslint-disable-next-line solid/no-innerhtml
+								innerHTML={`
 									(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 									new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 									j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 									'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 									})(window,document,'script','dataLayer','GTM-KDDWP2FS');
-								`}
+								`.trim()}
 								type="text/partytown"
 							/>
 						</>
