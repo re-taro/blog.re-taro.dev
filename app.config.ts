0ffb7de8f6fa9b28d8c5ae289fe751274aa04968
Rintaro Itokawa <me@re-taro.dev>
2025-03-29 11:44:47 +0900

    refactor: update partytown

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,8 @@
+import { join } from "node:path";
 import { defineConfig } from "@solidjs/start/config";
 import contentCollections from "@content-collections/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { partytownVite } from "@qwik.dev/partytown/utils";
 
 export default defineConfig({
 	server: {
@@ -14,6 +16,9 @@ export default defineConfig({
 		plugins: [
 			tsconfigPaths(),
 			contentCollections(),
+			partytownVite({
+				dest: join(import.meta.dirname, "dist", "~partytown"),
+			}),
 		],
 	},
 });
