7ed2e60af02dd654ebb0b18a0d9d4f99fab133f2
Rintaro Itokawa <me@re-taro.dev>
2025-05-18 01:45:20 +0900

    ci: update CD flow

--- apps/blog/vite.config.ts
+++ apps/blog/vite.config.ts
@@ -3,11 +3,14 @@ import { partytownVite } from '@qwik.dev/partytown/utils';
 import { redwood } from 'rwsdk/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
 	plugins: [
-		redwood(),
+		redwood({
+			// @ts-expect-error Type 'string' is not assignable to type '"development" | "production" | undefined'.ts(2322)
+			mode,
+		}),
 		partytownVite({
 			dest: path.join(import.meta.dirname, 'dist', 'client', '~partytown'),
 		}),
 	],
-});
+}));
